6d6b3af215ad341bbb9615dfb609cc1922305627
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2012-11-01 10:22:54 +0700

    0111

--- wp-content/themes/moex_v3/footer.php
+++ wp-content/themes/moex_v3/footer.php
@@ -15,9 +15,9 @@
                 <div class="fr"><a class="mnb" href="<?php echo get_bloginfo("url")?>?page_id=36" title="Giới thiệu">Giới thiệu</a></div>
 				<?php if(current_user_can("edit_posts")):?>
                 <div class="mnbSplit"><!----></div>
-                <div class="fr"><a class="mnb" href="<?php echo admin_url()?>">Admin</a></div>
+                <div class="fr"><a class="mnb" target="_blank" href="<?php echo admin_url()?>">Admin</a></div>
                 <div class="mnbSplit"><!----></div>
-                <div class="fr"><a class="mnb" href="<?php echo get_bloginfo("url")?>/erp/app.php/index">ERP</a></div>
+                <div class="fr"><a class="mnb" target="_blank" href="<?php echo get_bloginfo("url")?>/erp/app.php/index">ERP</a></div>
 				<?php endif;?>
                 <div class="cb"><!----></div>
             </div>            
--- wp-content/themes/moex_v3/search.php
+++ wp-content/themes/moex_v3/search.php
@@ -1,52 +1,32 @@
 <?php
 get_header();
 ?>
+	<?php 
+	$allsearch = &new WP_Query("s=$s&showposts=-1"); 
+	$count = $allsearch->post_count;
+	wp_reset_query(); ?>
     <div id="PageContent">
         <div id="KetQuaTimKiem">
            <div class="head"><!----></div>
-           <div class="fs12 pb15">Bạn vừa tìm kiếm từ khóa “<span class="cred">moEx</span>”, có 10 kết quả.</div>
+           <div class="fs12 pb15">Bạn vừa tìm kiếm từ khóa “<span class="cred"><?php echo get_search_query()?></span>”, có <?php echo $count;?> kết quả.</div>
+           <?php 
+				$k = 0;
+				while ( have_posts() ) : the_post(); 
+				$k++;
+			?>
+			<?php if ($k % 2 == 1):?>
            <div class="motkq fl">
-                <a class="title" href="#"><span>moEx</span> Go: Cùng bạn trên mọi chặng đường </a>
-                + Nhanh chóng: Sau khi bạn gọi 1900 56 56 36, moEx sẽ đón bạn trong vòng 10 phút<br />
-                + Thuận tiện: Dù bạn đang ở địa điểm nào, chỉ cần gọi, moEx sẽ đến tận nơi để phục vụ ...
+                <a class="title" href="<?php the_permalink();?>"><?php the_title();?></a>
+                <?php echo get_the_excerpt();?>
            </div>
+			<?php else:?>
+			
            <div class="motkq fr">
-                <a class="title" href="#"><span>moEx</span> Go: Cùng bạn trên mọi chặng đường </a>
-                + Nhanh chóng: Sau khi bạn gọi 1900 56 56 36, moEx sẽ đón bạn trong vòng 10 phút<br />
-                + Thuận tiện: Dù bạn đang ở địa điểm nào, chỉ cần gọi, moEx sẽ đến tận nơi để phục vụ ...
+                <a class="title" href="<?php the_permalink();?>"><?php the_title();?></a>
+                <?php echo get_the_excerpt();?>
            </div>
-           <div class="motkq fl">
-                <a class="title" href="#"><span>moEx</span> School: Cùng em tới trường </a>
-                + An tâm: Đội ngũ lái xe moEx được tuyển chọn kỹ lưỡng và đào tạo chuyên nghiệp, luôn đặt sự an toàn của khách hàng lên hàng đầu ...
-           </div>
-           <div class="motkq fr">
-                <a class="title" href="#"><span>moEx</span> School: Cùng em tới trường </a>
-                + An tâm: Đội ngũ lái xe moEx được tuyển chọn kỹ lưỡng và đào tạo chuyên nghiệp, luôn đặt sự an toàn của khách hàng lên hàng đầu ...
-           </div>
-           <div class="motkq fl">
-                <a class="title" href="#"><span>moEx</span> Food: Thêm hương vị cho cuộc sống</a>
-                + Tiện lợi: dù trời mưa, trời nắng, hay thời gian eo hẹp,  bạn vẫn có thể thưởng thức món ăn ưa thích ngay trong không gian quen thuộc của mình   ...
-           </div>
-           <div class="motkq fr">
-                <a class="title" href="#"><span>moEx</span> Food: Thêm hương vị cho cuộc sống</a>
-                + Tiện lợi: dù trời mưa, trời nắng, hay thời gian eo hẹp,  bạn vẫn có thể thưởng thức món ăn ưa thích ngay trong không gian quen thuộc của mình   ...
-           </div>
-           <div class="motkq fl">
-                <a class="title" href="#"><span>moEx</span> Shopping: Mọi chuyện đều trở nên đơn giản </a>
-                +Chủ động: Khi bạn không có thời gian mua sắm,  moEx sẽ giúp bạn có ngay những thứ bạn cần ...
-           </div>
-           <div class="motkq fr">
-                <a class="title" href="#"><span>moEx</span> Shopping: Mọi chuyện đều trở nên đơn giản </a>
-                +Chủ động: Khi bạn không có thời gian mua sắm,  moEx sẽ giúp bạn có ngay những thứ bạn cần ...
-           </div>
-           <div class="motkq fl">
-                <a class="title" href="#"><span>moEx</span> Office: tăng hiệu quả kinh doanh </a>
-                +Tốc độ: <b>moEx</b> hiểu trong kinh doanh thời gian là tiền bạc, mọi tài liệu và hàng hóa của bạn sẽ được chuyển tời đích trong vòng 2h từ khi <b>moEx</b> nhận hàng  ...
-           </div>
-           <div class="motkq fr">
-                <a class="title" href="#"><span>moEx</span> Office: tăng hiệu quả kinh doanh </a>
-                +Tốc độ: <b>moEx</b> hiểu trong kinh doanh thời gian là tiền bạc, mọi tài liệu và hàng hóa của bạn sẽ được chuyển tời đích trong vòng 2h từ khi <b>moEx</b> nhận hàng  ...
-           </div>
+			<?php endif;?>
+			<?php endwhile;?>
            <div class="cb h10"><!----></div>
         </div>
         </div>
