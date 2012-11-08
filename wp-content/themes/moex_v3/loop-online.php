ff619ce4298d140532adb0d2957485fa57b4dc38
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2012-11-08 09:31:54 +0700

    0811

--- wp-content/themes/moex_v3/loop-online.php
+++ wp-content/themes/moex_v3/loop-online.php
@@ -5,6 +5,6 @@
 	</ul></li>
 	<li><ul>
 	<li><a href="ymsgr:sendIM?cskh_moex"><img border="0" src="http://mail.opi.yahoo.com/online?u=cskh_moex&m=g&t=0"> CSKH moEx 3</a></li>
-	<li><a href="ymsgr:sendIM?small_angel140291"><img border="0" src="http://mail.opi.yahoo.com/online?u=small_angel140291&m=g&t=0"> CSKH moEx 4</a></li>
+	<li><a href="ymsgr:sendIM?cskh_moex_4"><img border="0" src="http://mail.opi.yahoo.com/online?u=cskh_moex_4&m=g&t=0"> CSKH moEx 4</a></li>
 	</ul></li>
 </ul>
