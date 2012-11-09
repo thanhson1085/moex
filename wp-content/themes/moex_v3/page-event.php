a24bc29439522532cc3532c2c2a0f5b5dc676301
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2012-11-09 18:08:23 +0700

    2011

--- wp-content/themes/moex_v3/page-event.php
+++ wp-content/themes/moex_v3/page-event.php
@@ -3,12 +3,14 @@
 ?>
         <div id="GioiThieu">
             <div id="mainAdv">
-                <a href="<?php get_bloginfo("url")?>/2011/"><img alt="" src="<?php echo get_bloginfo("template_url")?>/pic/20_11_2.jpg" class="anhQC"/></a>                
+                <a href="#"><img alt="" src="<?php echo get_bloginfo("template_url")?>/pic/20_11_2.jpg" class="anhQC"/></a>                
             </div>
             <div class="content">
                 <div class="pb10">
 					<span class="order-header" style="color:red; font-size: 28px;">Đăng ký dịch vụ</span>
                  </div>
+				<div style="postion; relative;overflow: hidden; height: 960px">
+				<table><tr><td style="width: 50%; vertical-align: top;">
 				<div style="background: white; position: relative; z-index: 10">
 					Chào mừng ngày nhà giáo Việt Nam 20/11, moEx mong muốn mang đến cho các em học sinh và gia đình một niềm vui nhỏ trong cuộc sống với chương trình: “Cùng moEx tới trường miễn phí”<br />
 					Nội dung: 
@@ -25,14 +27,15 @@
 					 	<li>1 tháng kể từ ngày đăng ký dịch vụ.</li>
 					</ul>
 					Địa điểm: Áp dụng với tất các quận nội thành Hà Nội.<br /><br />
-					Cách thức đăng ký: Quý phụ huynh vui lòng điền đầy đủ thông tin bên dưới hoặc gọi đến tổng đài: <b>1900 56 56 36</b>
+					Cách thức đăng ký: Quý phụ huynh vui lòng điền đầy đủ thông tin bên cạnh hoặc gọi đến tổng đài: <b>1900 56 56 36</b>
 
 				</div>
-				<div style="margin-top: -80px; z-index: 1">
-				<iframe src="https://docs.google.com/spreadsheet/embeddedform?formkey=dFlUSTZySHp0dUlsaUZ1QlpUVHc3V1E6MQ" width="760" height="1200" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+				</td><td style="width: 50%;">
+				<div style="margin-top: -87px;position: relative">
+				<iframe src="https://docs.google.com/spreadsheet/embeddedform?formkey=dFlUSTZySHp0dUlsaUZ1QlpUVHc3V1E6MQ" width="100%" height="1200" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
 				</div>
-				<style>
-				</style>
+				</td></tr></table>
+				</div>
 			</div>
         </div>
 <?php
