7e6595d04b7ac61c51a83f300d76a9ee75137ad6
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2012-11-29 11:10:32 +0700

    2911

--- wp-content/themes/4mobile/header.php
+++ wp-content/themes/4mobile/header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta name="viewport" content="initial-scale = 1.0,maximum-scale = 1.0" />
 <title></title>
 <link rel="stylesheet" type="text/css" href="<?php echo get_bloginfo("template_url");?>/style.css">
 <script src="<?php echo get_bloginfo("template_url")?>/js/jquery.min.js" type="text/javascript"></script>
