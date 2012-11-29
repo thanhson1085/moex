cdc0e52f4caaa50a396a3faed6089ac934929534
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2012-11-29 15:39:22 +0700

    2911

--- wp-content/themes/4mobile/footer.php
+++ wp-content/themes/4mobile/footer.php
@@ -1,4 +1,25 @@
 	</div>
 </div>
+<div class="footer-conatiner">
+	<div class="footer"></div>
+</div>
+<div class="menu-container">
+	<div class="menu">
+		<ul>
+			<li>
+				<a href="#">Tracking</a>
+			</li>
+			<li>
+				<a href="">Events</a>
+			</li>
+			<li>
+				<a href="">Services</a>
+			</li>
+			<li>
+				<a href="">About Us</a>
+			</li>
+		</ul>
+	</div>
+</div>
 </body>
 </html>
