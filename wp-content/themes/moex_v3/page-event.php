45355cf14d77b275e9c9aa9784b5e9220a40158e
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2012-11-30 17:38:15 +0700

    2011

--- wp-content/themes/moex_v3/page-event.php
+++ wp-content/themes/moex_v3/page-event.php
@@ -6,11 +6,13 @@
                 <a href="#"><img alt="" src="<?php echo get_bloginfo("template_url")?>/pic/ticket_980.jpg" class="anhQC"/></a>                
             </div>
             <div class="content">
+				<div class="event">
 				<?php
              		if ( have_posts() ) while ( have_posts() ) : the_post();
                         the_content();
                     endwhile;
                 ?>
+				</div>
 
 			</div>
         </div>
