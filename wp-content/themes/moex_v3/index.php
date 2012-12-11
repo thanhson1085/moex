ac9d8e0f98b25f78e7b00febf3c60250d0043ccf
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2012-12-11 08:54:50 +0700

    1112

--- wp-content/themes/moex_v3/index.php
+++ wp-content/themes/moex_v3/index.php
@@ -43,7 +43,7 @@ if ( function_exists( 'get_smooth_slider' ) ) { get_smooth_slider(); }
 					<div id="slideContainer">
 						<div id="slideShim">
 							<a href="<?php echo get_bloginfo("url");?>/xmas/"><img src="<?php echo get_bloginfo("template_url")?>/pic/home_xmas.jpg" alt="moEx Xmas" /></a>
-							<a href="<?php echo get_bloginfo("url");?>/event/"><img src="<?php echo get_bloginfo("template_url")?>/pic/banner_ticket.jpg" alt="Slide Two" /></a>
+							<a href="<?php echo get_bloginfo("url");?>/cinema/"><img src="<?php echo get_bloginfo("template_url")?>/pic/banner_ticket.jpg" alt="Slide Two" /></a>
 							<a href="<?php echo get_bloginfo("url");?>/order-search"><img src="<?php echo get_bloginfo("template_url")?>/pic/banner_8000.jpg" alt="Slide One" /></a>
 						</div>
 					</div>
