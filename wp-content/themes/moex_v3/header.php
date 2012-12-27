dc09d45d015491f06c44b09614892bcf63a317ea
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2012-12-27 12:09:52 +0700

    2712

--- wp-content/themes/moex_v3/header.php
+++ wp-content/themes/moex_v3/header.php
@@ -174,23 +174,23 @@ if (isMobile){
     
         <div id="MenuMain">
             <div class="fl">
-                <a class="mnm bgmnm0" href="<?php echo get_bloginfo("url")?>/moex-delivery/" title="Moex Delivery">Moex Delivery</a>            
+                <a class="mnm bgmnm0" href="<?php echo get_bloginfo("url")?>/moex-delivery/" title="Giao hàng nhanh trong 2 giờ">Moex Delivery</a>            
             </div>
             <div class="mnmSplit"><!----></div>
             <div class="fl">
-                <a class="mnm bgmnm1" href="<?php echo get_bloginfo("url")?>/moex-go/" title="Moex go">Moex go</a>            
+                <a class="mnm bgmnm1" href="<?php echo get_bloginfo("url")?>/moex-go/" title="Đưa đón hành khách mọi lúc mọi nơi">Moex go</a>            
             </div>
             <div class="mnmSplit"><!----></div>
             <div class="fl">
-                <a class="mnm bgmnm2" href="<?php echo get_bloginfo("url")?>/moex-food/" title="Moex food">Moex food</a>            
+                <a class="mnm bgmnm2" href="<?php echo get_bloginfo("url")?>/moex-food/" title="Mua đồ ăn nhanh phong phú đa dạng">Moex food</a>            
             </div>
             <div class="mnmSplit"><!----></div>
             <div class="fl">
-                <a class="mnm bgmnm3" href="<?php echo get_bloginfo("url")?>/moex-shopping/" title="Moex shopping">Moex shopping</a>            
+                <a class="mnm bgmnm3" href="<?php echo get_bloginfo("url")?>/moex-shopping/" title="Mua sắm nhanh chóng tiện lợi">Moex shopping</a>            
             </div>
             <div class="mnmSplit"><!----></div>
             <div class="fl">
-                <a class="mnm bgmnm4" href="<?php echo get_bloginfo("url")?>/moex-school/" title="Moex school">Moex school</a>            
+                <a class="mnm bgmnm4" href="<?php echo get_bloginfo("url")?>/moex-school/" title="Đưa đón học sinh an toàn chu đáo">Moex school</a>            
             </div>
             <div class="cb h10"><!----></div>
         </div>
