a73c0cab9a5d792b2137df1f990804c39e638f24
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2013-01-09 12:22:12 +0700

    0901

--- wp-content/themes/moex_v3/header.php
+++ wp-content/themes/moex_v3/header.php
@@ -11,12 +11,14 @@
     wp_title( '-', true, 'right' );
 
     // Add the blog name.
-    bloginfo( 'name' );
+    //bloginfo( 'name' );
 
 // Add the blog description for the home/front page.
+/*
 $site_description = get_bloginfo( 'description', 'display' );
 if ( $site_description && ( is_home() || is_front_page() ) )
 	echo " - $site_description";
+*/
 ?>
 </title>
 <link href="<?php echo get_bloginfo("template_url")?>/pic/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
