6e827854b31c0875708f085224105a9c55855335
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2013-01-14 12:11:55 +0700

    1401

--- wp-content/themes/local/functions.php
+++ wp-content/themes/local/functions.php
@@ -35,3 +35,15 @@ $role = get_role('cs_router');
 $role->add_cap('edit_pages');
 $role->add_cap('edit_others_pages');
 
+function custom_login_logo() {
+    echo '<style type="text/css">
+    h1 a { background-image: url('.get_bloginfo('template_directory').'/images/logo3.png) !important; background-size: 200px !important}
+    </style>';
+}
+add_action('login_head', 'custom_login_logo');
+add_filter( 'show_admin_bar', '__return_false' );
+function my_login_redirect($redirect_to, $request){
+	return home_url(); 
+}
+add_filter("login_redirect", "my_login_redirect", 10, 3);
+
